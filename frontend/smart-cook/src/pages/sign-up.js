0d0dbb1bac26335770ed13912740daddf6b23e3e
MZhann <zhanbolat.mukan2004@gmail.com>
2024-03-26 17:06:46 +0500

    feat: add sign-up.js page

--- frontend/smart-cook/src/pages/sign-up.js
+++ frontend/smart-cook/src/pages/sign-up.js
@@ -3,18 +3,35 @@ import logo from "../../public/images/SmartCookLogo.png";
 import Image from "next/image";
 import Link from "next/link";
 
-const SignIn = () => {
+const SignUp = () => {
     return (
         <div className="w-full flex flex-col items-center bg-[#2A293B] min-h-screen ">
             <Image src={logo} alt="logo" className="w-[300px] mt-6 mb-8" />
-            <div className="w-[450px] h-[376px] bg-white rounded-xl flex justify-center">
+            <div className="w-[450px] h-[556px] bg-white rounded-xl flex justify-center">
                 <div className="w-8/12 mt-8 flex flex-col">
-                    <h1 className="text-lg font-bold">Log in</h1>
+                    <h1 className="text-lg font-bold">Sign up</h1>
                     <p className="text-xs mt-1">
-                        New user?
-                        <Link href="#" className="text-[#80CC2D] hover:border-b-[#AAE06E] border-white border-b-2">&nbsp;Sign up&nbsp;</Link>
+                        Already have an account?
+                        <Link href="#" className="text-[#80CC2D] hover:border-b-[#AAE06E] border-white border-b-2">&nbsp;Log in&nbsp;</Link>
                     </p>
-                    <p className="text-sm mt-5">Enter your email</p>
+                    <p className="text-sm mt-5">First name</p>
+                    <input
+                        id="name"
+                        className="w-full rounded-3xl border-2 h-10 shadow-gray-500 text-xs p-3 mt-2"
+                        placeholder="name"
+                        type="name"
+                        // value={field}
+                        // onChange={(e) => setField(e.target.value)}
+                    />
+                    <p className="text-sm mt-5">Last name</p>
+                    <input
+                        id="surname"
+                        type="surname"
+                        className="w-full rounded-3xl border-2 h-10 shadow-gray-500 text-xs p-3 mt-2"
+                        placeholder="surname"
+                        
+                    />
+                    <p className="text-sm mt-5">Email</p>
                     <input
                         id="email"
                         className="w-full rounded-3xl border-2 h-10 shadow-gray-500 text-xs p-3 mt-2"
@@ -23,7 +40,7 @@ const SignIn = () => {
                         // value={field}
                         // onChange={(e) => setField(e.target.value)}
                     />
-                    <p className="text-sm mt-5">Enter your password</p>
+                    <p className="text-sm mt-5">Creat a password</p>
                     <input
                         id="password"
                         type="password"
@@ -39,4 +56,4 @@ const SignIn = () => {
     );
 };
 
-export default SignIn;
+export default SignUp;
