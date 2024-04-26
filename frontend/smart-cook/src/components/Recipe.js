c11aba9704fa8f534fcc140c4d38de27b17a2e80
Temirlan Abeu <temabeu2003@gmail.com>
2024-04-26 23:39:11 +0500

    fix: fix recipe cards, add favorites into localstorage

--- frontend/smart-cook/src/components/Recipe.js
+++ frontend/smart-cook/src/components/Recipe.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Image from "next/image";
 import defaultFood from "@/../public/images/defaultFood.png";
 import Like from "@/components/Like";
@@ -6,14 +6,22 @@ import axios from "axios";
 import aiLogo from "@/../public/images/ai-logo.png";
 import {useRouter} from "next/router";
 import Link from "next/link";
+import saved from "@/../public/images/bluesaved.png"
 
 const Recipe = ({recipe}) => {
     const router = useRouter();
     const [isHovered, setIsHovered] = useState(false);
-    const [favButtonClicked, setFavButtonClicked] = useState(false);
+    const [isFavorite, setIsFavorite] = useState(false);
 
+    useEffect(() => {
+        const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+        const found = favoriteRecipes.some(favRecipe => favRecipe.id === recipe.id);
+        setIsFavorite(found);
+    }, [recipe.id]);
 
-    const handleFavClick = async () => {
+    const handleFavClick = async (event) => {
+        event.preventDefault();
+
         try {
             const config = {
                 headers: {
@@ -24,9 +32,35 @@ const Recipe = ({recipe}) => {
                 `https://web-production-ad96.up.railway.app/api/v1/recipes/${recipe.id}/add_to_favorites/`, recipe.id, config
             );
 
-            if (response.status === 201) {
-                console.log(response.data);
-                setFavButtonClicked(true);
+            if (response.status === 200) {
+                const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+                if (!favoriteRecipes.some(favRecipe => favRecipe.id === recipe.id)) {
+                    favoriteRecipes.push(recipe);
+                    localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipes));
+                    setIsFavorite(true);
+                }
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const handleRemoveFromFavClick = async () => {
+        try {
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+                },
+            };
+            const url = `https://web-production-ad96.up.railway.app/api/v1/recipes/${recipe.id}/remove_from_favorites/`;
+
+            const response = await axios.post(url, recipe.id, config);
+
+            if (response.status === 200) {
+                const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+                const updatedFavoriteRecipes = favoriteRecipes.filter(favRecipe => favRecipe.id !== recipe.id);
+                localStorage.setItem('favoriteRecipes', JSON.stringify(updatedFavoriteRecipes));
+                setIsFavorite(false);
             }
         } catch (error) {
             console.error(error);
@@ -34,7 +68,6 @@ const Recipe = ({recipe}) => {
     };
 
     return (
-
         <div className="w-[280px] h-[340px] bg-white rounded-lg my-5 ">
             <div className="w-full h-[210px] relative">
                 {recipe.world_cuisine && recipe.dish_type &&
@@ -51,28 +84,29 @@ const Recipe = ({recipe}) => {
                      onMouseLeave={() => setIsHovered(false)}
                      onClick={(event) => {
                          event.stopPropagation();
-                         handleFavClick();
+                         handleFavClick().catch(err => console.error(err));
                      }}
                 >
-                    <svg
-                        className={`hover:cursor-pointer ${!favButtonClicked ? "fill-none" : 'fill-[#4c9fed]'}`}
-                        width="35"
-                        height="35"
-                        viewBox="0 0 35 35"
-
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <rect width="35" height="35" rx="17.5" fill={!favButtonClicked ? "white" : '#4c9fed'}
-                              fillOpacity="0.8"/>
-                        <path
-                            d="M21.5166 8.75H13.4833C11.7083 8.75 10.2666 10.2725 10.2666 12.1275V24.4563C10.2666 26.0313 11.3416 26.6962 12.6583 25.935L16.7249 23.5638C17.1583 23.31 17.8583 23.31 18.2833 23.5638L22.3499 25.935C23.6666 26.705 24.7416 26.04 24.7416 24.4563V12.1275C24.7333 10.2725 23.2916 8.75 21.5166 8.75Z"
-                            className={`stroke-black stroke-2 ${isHovered ? 'stroke-blue-400' : ''}`}
-                            strokeWidth="1.5"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            style={{transition: 'stroke 0.3s ease'}}
-                        />
-                    </svg>
+                    {recipe && router.pathname !== '/ai-receipts' &&
+                        <>
+                            {!isFavorite ?
+                                <button onClick={(event) => handleFavClick(event)}>
+                                    <svg width="35" height="35" viewBox="0 0 35 35" fill="none"
+                                         xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="35" height="35" rx="17.5" fill="white" fillOpacity="0.8"/>
+                                        <path
+                                            d="M21.5166 8.75H13.4833C11.7083 8.75 10.2666 10.2725 10.2666 12.1275V24.4563C10.2666 26.0313 11.3416 26.6962 12.6583 25.935L16.7249 23.5638C17.1583 23.31 17.8583 23.31 18.2833 23.5638L22.3499 25.935C23.6666 26.705 24.7416 26.04 24.7416 24.4563V12.1275C24.7333 10.2725 23.2916 8.75 21.5166 8.75Z"
+                                            stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                                    </svg>
+                                </button>
+                                :
+                                <button onClick={() => handleRemoveFromFavClick()}>
+                                    <Image src={saved} alt={'saved'}
+                                           className={`size-[35px]`}/>
+                                </button>
+                            }
+                        </>
+                    }
                 </div>
                 <Link href="/recipes/[recipeTitle]" as={`/recipes/${recipe.id}`} passHref>
                     {recipe && recipe.image ?
