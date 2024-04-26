0fd1c58385fe84ba37af48aa65bab9db9ea1c948
MZhann <zhanbolat.mukan2004@gmail.com>
2024-04-26 23:51:53 +0500

    fix: Footer fix, call to whats app, map to Almaty 2Gis, Email

--- frontend/smart-cook/src/components/Footer.js
+++ frontend/smart-cook/src/components/Footer.js
@@ -6,6 +6,16 @@ import phone from "../../public/images/phone.png";
 import location from "../../public/images/location.png";
 
 const Footer = () => {
+    const handleLocationClick = () => {
+        // Construct the 2GIS URL with the location parameters for Almaty, Kazakhstan
+        const latitude = 43.238949;
+        const longitude = 76.889709;
+        const url = `https://2gis.kz/almaty/search/${latitude},${longitude}/zoom/17`;
+    
+        // Open the location in 2GIS
+        window.open(url, '_blank');
+      };
+
     return (
         <div className="lg: px-6">
             <div className="hidden w-full text-white lg:flex h-[300px] mt-10">
@@ -30,16 +40,24 @@ const Footer = () => {
                     </Link>
                 </div>
                 <div className="h-full w-3/12 border-r-2 border-gray-100 flex flex-col justify-center items-center">
-                    <Link className="flex flex-col items-center justify-center" href="tel:+77473433434">
+                    <Link
+                        className="flex flex-col items-center justify-center"
+                        href="https://wa.me/77473433434"
+                    >
                         <Image src={phone} alt="phone" />
                         <div className="text-lg mt-3">CALL</div>
                         <div className="text-xs">+7(747)-343-34-34</div>
                     </Link>
                 </div>
                 <div className="h-full w-2/12 flex flex-col justify-center items-end">
-                    <Image src={location} alt="mail" />
-                    <div className="text-lg mt-3">FIND US</div>
-                    <div className="text-xs">Almaty, KZ</div>
+                    <button
+                        onClick={handleLocationClick}
+                        
+                    >
+                        <Image src={location} alt="location" />
+                        <div className="text-lg mt-3">FIND US</div>
+                        <div className="text-xs">Almaty, KZ</div>
+                    </button>
                 </div>
             </div>
             <div className="flex sm:space-x-96 text-white mt-14 mb-5">
